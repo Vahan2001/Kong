e0d93d0f7884253b559823a975266702e1cc0c9e
andranik92nersisyan <nersisyan9292@gmail.com>
2024-11-29 20:04:14 +0400

    new Branch add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./index.css";
 import "./App.css";
 import HeroSection from "./components/heroSection/heroSection";
+import FeaturesShowcase from "./components/featuresShowcase/featuresShowcase";
 
 function App() {
   return (
     <div className="App">
-      <HeroSection />
+      <FeaturesShowcase/>
     </div>
   );
 }
