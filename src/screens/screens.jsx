dc19a98b2508bc245417d4043fa511e07e2db70b
andranik92nersisyan <nersisyan9292@gmail.com>
2024-11-29 20:54:35 +0400

    create new styles

--- src/screens/screens.jsx
+++ src/screens/screens.jsx
@@ -1,11 +1,13 @@
 import CityOverview from "../components/cityOverview/cityOverview";
-import HeroSection from "../components/heroSection/heroSection";
+// import HeroSection from "../components/heroSection/heroSection";
+import FeaturesShowcase from "../components/featuresShowcase/featuresShowcase"
 
 export default function Screens() {
   return (
     <>
-      <HeroSection />
+      {/* <HeroSection /> */}
       <CityOverview />
+      <FeaturesShowcase/>
     </>
   );
 }
