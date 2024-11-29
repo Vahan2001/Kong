5e41ecb1e954079ac3798168d3969982b4a8fc54
Paylak <paylakabrahamyan15@gmail.com>
2024-11-29 20:51:23 +0400

    add fourth section

--- src/components/cityOverview/cityOverview.jsx
+++ src/components/cityOverview/cityOverview.jsx
@@ -5,12 +5,7 @@ export default function CityOverview() {
     <div className="container">
       <div className={style.heroSection}>
         <div className={style.content}>
-          <span className={style.textLayer}>
-            <span className={style.orange_text}>BANANA ZONE</span> is waiting
-            for a worthy one who <br /> can lead the market to greatness....
-            <br />
-            <span className={style.orange_text}>AND KONG WILL COME!!</span>
-          </span>
+          <span className={style.textLayer}><span className={style.orange_text}>BANANA ZONE</span> is waiting for a worthy one who <br/> can lead the market to greatness....<br/> <span className={style.orange_text}>AND KONG WILL COME!!</span></span>
         </div>
       </div>
     </div>
