4279145d9cc8afbf5a13c65917c6e3f91b6b2691
Vahan2001 <vahan.avetisyan.2018@mail.ru>
2024-11-30 13:01:20 +0400

    update media footer

--- src/screens/screens.jsx
+++ src/screens/screens.jsx
@@ -1,15 +1,16 @@
 import CityOverview from "../components/cityOverview/cityOverview";
 import HeroSection from "../components/heroSection/heroSection";
-import FeaturesShowcase from "../components/featuresShowcase/featuresShowcase"
+import FeaturesShowcase from "../components/featuresShowcase/featuresShowcase";
+import FooterSection from "../components/footerSection/footerSection";
+import StatisticsPanel from "../components/statisticsPanel/statisticsPanel";
 export default function Screens() {
   return (
     <>
       <HeroSection />
       <CityOverview />
-      <FeaturesShowcase/>
-      <FooterSection />
       <FeaturesShowcase />
       <StatisticsPanel />
+      <FooterSection />
     </>
   );
 }
