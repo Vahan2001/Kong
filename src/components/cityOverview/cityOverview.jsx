b3c10ed75979f45a50002fd583e7a4cabc184171
Paylak <paylakabrahamyan15@gmail.com>
2024-12-06 04:12:15 +0400

    add media

--- src/components/cityOverview/cityOverview.jsx
+++ src/components/cityOverview/cityOverview.jsx
@@ -44,13 +44,6 @@ export default function CityOverview() {
              <Lottie options={lottieOptions}/>
           </div>
           <div className={style.content} ref={contentRef}>
-            
-            {/* <span className={style.textLayer}>
-              <span className={style.orange_text}>BANANA ZONE</span> is waiting
-              for a worthy one who <br /> can lead the market to greatness....
-              <br />
-              <span className={style.orange_text}>AND KONG WILL COME!!</span>
-            </span> */}
           </div>
         </div>
       </div>
